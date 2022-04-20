4f3a6bef0bd2f233cdc6b7402cff7be11077092f
alfazari <faa04807@gmail.com>
2022-04-20 07:51:03 +0700

    idk what i do

--- src/Component/Form/__test__/index.test.js
+++ src/Component/Form/__test__/index.test.js
@@ -1,35 +1,29 @@
-import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Form } from '../index';
+import { store } from '../../../store/store';
 import { Provider } from 'react-redux';
 
-const setup = () => {
-    render(
-        <Provider store={store}>
-            <Form />
-        </Provider>
-    )
-}
 
-describe('Form Create Playlist should be render', () => {
-    beforeEach(setup);
-    afterEach(cleanup);
+
+test('it should render login button properly', () => {
+  render(<Provider store={store}>
+    <Form />
+  </Provider>);
+
   
-    it('Success render Create Playlist Form', () => {
-      expect(screen.getByLabelText('title')).toBeInTheDocument();
-      expect(screen.getByLabelText('description')).toBeInTheDocument();
-      expect(screen.getByText('Create')).toBeInTheDocument();
-    });
+    
+  expect(screen.getByLabelText('title')).toBeInTheDocument();
+  expect(screen.getByLabelText('description')).toBeInTheDocument();
+  expect(screen.getByText('Create')).toBeInTheDocument();
   
-    it('Type in form Create Playlist', () => {
-      const titleInput = screen.getByLabelText('title');
-      const descriptionInput = screen.getByLabelText('description');
+  const titleInput = screen.getByLabelText('title');
+  const descriptionInput = screen.getByLabelText('description');
   
-      userEvent.type(titleInput, 'Title');
-      userEvent.type(descriptionInput, 'Description');
+  userEvent.type(titleInput, 'Title');
+  userEvent.type(descriptionInput, 'Description');
   
-      expect(titleInput).toHaveValue('Title');
-      expect(descriptionInput).toHaveValue('Description');
-    });
-  });
+  expect(titleInput).toHaveValue('Title');
+  expect(descriptionInput).toHaveValue('Description');
+    
+});
